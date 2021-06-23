506f18f9140b84062ade43b35c2424c8de98f87d
Stratos Pattas <stratos.pattas@outlook.com.gr>
2021-06-23 22:45:25 +0300

    Buttons functionalities

--- Calculator/MainWindow.xaml.cs
+++ Calculator/MainWindow.xaml.cs
@@ -22,16 +22,22 @@ namespace Calculator
     {
         double lastNumber;
         double result;
+        private SelectedOperator selectedOperator;
 
         public MainWindow()
         {
             InitializeComponent();
             acBtn.Click += AcBtn_Click;
-            negativeBtn.Click += NegativeBtn_Click;          
+            negativeBtn.Click += NegativeBtn_Click;
             equalBtn.Click += EqualBtn_Click;
             percentageBtn.Click += PercentageBtn_Click;
         }
 
+        /// <summary>
+        /// Handles the Click event of the "+/-" Button. It changes the sign of the displayed number.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="RoutedEventArgs"/> instance containing the event data.</param>
         private void NegativeBtn_Click(object sender, RoutedEventArgs e)
         {
             if (double.TryParse(resultLabel.Content.ToString(), out lastNumber))
@@ -41,33 +47,94 @@ namespace Calculator
             }
         }
 
+        /// <summary>
+        /// Handles the Click event of the "%" Button.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="RoutedEventArgs"/> instance containing the event data.</param>
         private void PercentageBtn_Click(object sender, RoutedEventArgs e)
         {
-            if (double.TryParse(resultLabel.Content.ToString(), out lastNumber))
+            double tempNumber;
+            if (double.TryParse(resultLabel.Content.ToString(), out tempNumber))
             {
-                lastNumber = lastNumber / 100;
-                resultLabel.Content = lastNumber.ToString();
+                tempNumber /= 100;
+                if (lastNumber != 0)
+                    tempNumber *= lastNumber;
+                resultLabel.Content = tempNumber.ToString();
             }
         }
 
+        /// <summary>
+        /// Handles the Click event of the "=" Button.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="RoutedEventArgs"/> instance containing the event data.</param>
         private void EqualBtn_Click(object sender, RoutedEventArgs e)
         {
-            
+            double newNumber;
+            if (double.TryParse(resultLabel.Content.ToString(), out newNumber))
+            {
+                switch (selectedOperator)
+                {
+                    case SelectedOperator.Addition:
+                        result = SimpleMath.Add(lastNumber, newNumber);
+                        break;
+                    case SelectedOperator.Sustraction:
+                        result = SimpleMath.Sustraction(lastNumber, newNumber);
+                        break;
+                    case SelectedOperator.Multiplication:
+                        result = SimpleMath.Multiply(lastNumber, newNumber);
+                        break;
+                    case SelectedOperator.Division:
+                        result = SimpleMath.Divide(lastNumber, newNumber);
+                        break;
+                }
+
+                resultLabel.Content = result.ToString();
+            }
         }
 
+        /// <summary>
+        /// Handles the Click event of the "AC" Button.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="System.Windows.RoutedEventArgs"/> instance containing the event data.</param>
+        /// <returns></returns>
         private void AcBtn_Click(object sender, RoutedEventArgs e)
         {
             resultLabel.Content = "0";
         }
 
+        /// <summary>
+        /// Handles the Click event of the OperationButton control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="System.Windows.RoutedEventArgs"/> instance containing the event data.</param>
+        /// <returns></returns>
         private void OperationButton_Click(object sender, RoutedEventArgs e)
         {
             if (double.TryParse(resultLabel.Content.ToString(), out lastNumber))
             {
                 resultLabel.Content = "0";
             }
+
+
+            if (sender == multiplyBtn)
+                selectedOperator = SelectedOperator.Multiplication;
+            if (sender == divisionBtn)
+                selectedOperator = SelectedOperator.Division;
+            if (sender == plusBtn)
+                selectedOperator = SelectedOperator.Addition;
+            if (sender == minusBtn)
+                selectedOperator = SelectedOperator.Sustraction;
         }
 
+        /// <summary>
+        /// Handles the Click event of the number buttons.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="System.Windows.RoutedEventArgs"/> instance containing the event data.</param>
+        /// <returns></returns>
         private void NumberButton_Click(object sender, RoutedEventArgs e)
         {
             int selectedValue = int.Parse((sender as Button).Content.ToString());
@@ -77,6 +144,58 @@ namespace Calculator
             else
                 resultLabel.Content = $"{resultLabel.Content}{selectedValue}";
         }
+
+        /// <summary>
+        /// Handles the Click event of the "." button.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="System.Windows.RoutedEventArgs"/> instance containing the event data.</param>
+        /// <returns></returns>
+        private void pointBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (!resultLabel.Content.ToString().Contains("."))
+            {
+                resultLabel.Content = $"{resultLabel.Content}.";
+            }
+        }
+
+        public enum SelectedOperator
+        {
+            Addition,
+            Sustraction,
+            Multiplication,
+            Division
+        }
+
+        public class SimpleMath
+        {
+            public static double Add(double n1, double n2)
+            {
+                return n1 + n2;
+            }
+
+            public static double Sustraction(double n1, double n2)
+            {
+                return n1 - n2;
+            }
+
+            public static double Multiply(double n1, double n2)
+            {
+                return n1 * n2;
+            }
+
+            public static double Divide(double n1, double n2)
+            {
+                if (n2 == 0)
+                {
+                    MessageBox.Show("Division by 0 is not supported", "Wrong operation", MessageBoxButton.OK, MessageBoxImage.Error);
+                    return 0;
+                }
+                return n1 / n2;
+            }
+        }
+
+        
     }
 
     
